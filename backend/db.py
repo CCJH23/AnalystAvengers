b9b269b4a5252338639b81ca696e978f22734994
clovischow <clovischowjh@gmail.com>
2023-12-29 17:49:26 +0800

    Update dependencies and added cert for mongo

--- backend/db.py
+++ backend/db.py
@@ -1,9 +1,9 @@
 from flask_pymongo import pymongo
 import os
+import certifi
 
 CONNECTION_STRING = os.getenv('MONGO_CONNECTION_STRING')
 
-client = pymongo.MongoClient(CONNECTION_STRING)
+client = pymongo.MongoClient(CONNECTION_STRING, tlsCAFile=certifi.where())
 db = client.get_database('testdb')
 user_collection = pymongo.collection.Collection(db, 'test_collection')
-

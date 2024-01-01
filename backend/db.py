b69527a455d78e7abec0606531342de292f0327a
clovischow <clovischowjh@gmail.com>
2024-01-01 23:06:52 +0800

    Downgraded to Python 3.11 and removed cert

--- backend/db.py
+++ backend/db.py
@@ -4,6 +4,6 @@ import certifi
 
 CONNECTION_STRING = os.getenv('MONGO_CONNECTION_STRING')
 
-client = pymongo.MongoClient(CONNECTION_STRING, tlsCAFile=certifi.where())
+client = pymongo.MongoClient(CONNECTION_STRING)
 db = client.get_database('testdb')
 user_collection = pymongo.collection.Collection(db, 'test_collection')

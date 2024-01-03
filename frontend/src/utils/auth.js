7df2b553d88f5091efd2745aae25dd1f11120499
Aloysius Tan <69228664+narnia1601@users.noreply.github.com>
2024-01-03 12:13:49 +0800

    added register authentication

---
+++ frontend/src/utils/auth.js
@@ -0,0 +1,28 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
+const firebaseConfig = {
+    apiKey: import.meta.env.VITE_APP_API_KEY,
+    authDomain: import.meta.env.VITE_APP_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_APP_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_APP_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_APP_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_APP_APP_ID,
+    measurementId: import.meta.env.VITE_APP_MEASUREMENT_ID
+};
+
+initializeApp(firebaseConfig);
+
+class Auth {
+    async register(email, password) {
+        try {
+            const auth = getAuth()
+            const userCredential = await createUserWithEmailAndPassword(auth, email, password)
+            return userCredential.user
+        } catch (error) {
+            throw error;
+        }
+    }
+}
+
+export default new Auth()
